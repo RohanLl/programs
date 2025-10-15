40c4df3351a671401259a744e2d747f208c464cb
RohanLl <rohan170208@gmail.com>
2025-10-15 22:30:27 +0530

    sumne

---
+++ practise.java
@@ -0,0 +1,23 @@
+public class practise {
+
+    public static void main(String[] args) {
+        // Initialize integer array
+        int[] arr = new int[5];
+        // Initialize float array with values
+        float[] arr1 = {1, 2, 3, 4, 5};
+
+        // String initialization (prefer direct assignment)
+        String str = "JAVA PROGRAM";
+
+        // Print elements of float array
+        for (float e : arr1) {
+            System.out.println(e);
+        }
+        // Optionally, print elements of integer array
+        for (int e : arr) {
+            System.out.println(e);
+        }
+    
+    }
+    
+}
